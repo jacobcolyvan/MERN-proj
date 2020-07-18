4d3ee195a386c9dca0071f8111287016850d0318
Andrew Tran <andrewtttran@gmail.com>
2020-07-18 23:20:39 +1000

    add logic for username already taken

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,14 +1,13 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-const userModel = require('../models/user')
+const userModel = require('../models/user');
 const {
   getUsers,
   getUser,
   newUser,
   updateUser,
-  deleteUser
-} = require('../controllers/users_controller')
-
+  deleteUser,
+} = require('../controllers/users_controller');
 
 // Get all
 router.get('/users', async (req, res) => {
@@ -17,51 +16,52 @@ router.get('/users', async (req, res) => {
   try {
     res.send(users);
   } catch (err) {
-    console.log("there was an error");
+    console.log('there was an error');
     res.status(500).send(err);
   }
-})
+});
 
 router.get('/user/:id', async (req, res) => {
   try {
-    const user = await userModel.findById(req.params.id)
+    const user = await userModel.findById(req.params.id);
     // if (!user) res.status(404).send("No user here")
-    res.send(user)
+    res.send(user);
   } catch {
-    res.status(500).send(err)
-    console.log("Not a valid user");
+    res.status(500).send(err);
+    console.log('Not a valid user');
   }
-
-})
-
+});
 
 // Create
 router.post('/user', async (req, res) => {
   const user = new userModel(req.body);
 
+  const { username, password } = req.body;
+
   try {
+    let check = await userModel.findOne({ username });
+    if (check) {
+      res.status(400).json({ errors: [{ msg: 'username already taken' }] });
+    }
+
     await user.save();
-    res.send(user)
+    res.send(user);
   } catch (err) {
-    console.log("there was an error");
-    res.status(500).send(err)
+    console.log('there was an error');
+    res.status(500).send(err);
   }
-})
-
-
+});
 
 router.delete('/user/:id', async (req, res) => {
   try {
-    const user = await userModel.findByIdAndDelete(req.params.id)
+    const user = await userModel.findByIdAndDelete(req.params.id);
 
-    if (!user) res.status(404).send("No user here")
-    res.status(200).send()
+    if (!user) res.status(404).send('No user here');
+    res.status(200).send();
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
-})
-
-
+});
 
 // Update for recipes
 router.put('/user/:id', async (req, res) => {
@@ -72,17 +72,15 @@ router.put('/user/:id', async (req, res) => {
 
     const user = await userModel.findById(req.params.id);
     console.log(req.body.newRecipe);
-    newRecipes = [...user.recipes, req.body.newRecipe]
-    await user.update({ recipes: newRecipes })
-    res.send(newRecipes)
+    newRecipes = [...user.recipes, req.body.newRecipe];
+    await user.update({ recipes: newRecipes });
+    res.send(newRecipes);
     // res.send('Recipe added to user')
   } catch (err) {
-    console.log("no way mon");
-    res.status(400).send(err)
+    console.log('no way mon');
+    res.status(400).send(err);
   }
-})
-
-
+});
 
 //clear all users
 // router.delete('/users', async (req, res) => {
@@ -94,7 +92,4 @@ router.put('/user/:id', async (req, res) => {
 //   }
 // })
 
-
-
-module.exports = router
-
+module.exports = router;
